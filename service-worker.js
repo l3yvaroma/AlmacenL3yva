a1d7a950baac392383c4380c4c9e9bf016c1f738
l3yvaroma <romarioleyvam@gmail.com>
2026-08-11 17:06:02 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,35 +1,7 @@
-const CACHE_NAME = 'almacenpro-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.webmanifest',
-  './icon-192.png',
-  './icon-512.png'
-];
-
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)));
-  self.skipWaiting();
-});
-
+// Service worker LIMPIO: se autoelimina y borra cachés antiguas.
+self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((ns) =>
-      Promise.all(ns.filter((n) => n !== CACHE_NAME).map((n) => caches.delete(n)))
-    ).then(() => self.clients.claim())
-  );
+  e.waitUntil(caches.keys().then(k => Promise.all(k.map(c => caches.delete(c))))
+    .then(() => self.registration.unregister()).then(() => self.clients.claim()));
 });
-
-self.addEventListener('fetch', (e) => {
-  if (e.request.method !== 'GET') return;
-  e.respondWith(
-    caches.match(e.request).then((cached) => {
-      if (cached) return cached;
-      return fetch(e.request).then((res) => {
-        const copy = res.clone();
-        caches.open(CACHE_NAME).then((c) => c.put(e.request, copy));
-        return res;
-      });
-    }).catch(() => caches.match('./index.html'))
-  );
-});
+self.addEventListener('fetch', (e) => e.respondWith(fetch(e.request, { cache: 'no-store' })));
